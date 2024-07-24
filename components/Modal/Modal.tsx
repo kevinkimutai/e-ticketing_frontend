3012c67616ac05979e25ed8257f7a16a7a75a736
kevin-kimutai <kevinkimutai62@gmail.com>
2024-07-24 23:34:04 +0300

    event modal

---
+++ components/Modal/Modal.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+type ComponentProps = {
+  title: string;
+  description: string;
+};
+
+const Modal = ({ title, description }: ComponentProps) => {
+  return <div></div>;
+};
+
+export default Modal;

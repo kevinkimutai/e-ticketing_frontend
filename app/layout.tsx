4903b8c4aa53b55f494757619788ad5b27c63849
kevin-kimutai <kevinkimutai62@gmail.com>
2024-08-15 20:04:46 +0300

    fix firebase import

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
-import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
